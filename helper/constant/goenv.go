eba8bf0e9454c68d8ae1befdff87b4449bc3cfd7
jannes-sa <jannes.sa@tnis.com>
2018-10-03 19:34:13 +0700

    adding component test genfile in routergen

--- helper/constant/goenv.go
+++ helper/constant/goenv.go
@@ -5,6 +5,7 @@ var (
 	DEVCI         = "devci"
 	KUBE          = "kube"
 	DOMAIN        = "01"
+	DOMAINNAME    = "template"
 	SVC           = "01"
 	PREFIXERRCODE = "E" + DOMAIN + SVC
 )
--- routers/componenttest/http/template_test.go
+++ routers/componenttest/http/template_test.go
@@ -1,8 +1,12 @@
 package http
 
 import (
+	"encoding/json"
 	"strconv"
+	"template/helper/constant"
 	"template/routers/componenttest"
+	"template/structs"
+	httpStructs "template/structs/api/http"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -10,42 +14,27 @@ import (
 	"github.com/astaxie/beego"
 )
 
+var (
+	host string = "http://127.0.0.1:" + strconv.Itoa(beego.BConfig.Listen.HTTPPort)
+)
+
 func TestTemplateSuccess(t *testing.T) {
-	host := "http://127.0.0.1:" + strconv.Itoa(beego.BConfig.Listen.HTTPPort)
+
+	var req structs.ReqData
+	var reqBody httpStructs.ReqTest
+	reqBody.ID = 1
+	req.ReqBody = reqBody
+	by, _ := json.Marshal(req)
 
 	res := componenttest.SendHTTP(
 		"POST",
-		host+"/template/v1/template",
-		[]byte(`{
-			"rqBody":{
-				"id":1
-			}
-		}`),
+		host+"/"+constant.DOMAINNAME+"/v1/template",
+		by,
 	)
 
 	Convey("TestTemplateSuccess", t, func() {
 		Convey("Should Success", func() {
 			So(len(res.Error), ShouldEqual, 0)
-		})
-	})
-
-}
-
-func TestTemplateFailed(t *testing.T) {
-	host := "http://127.0.0.1:" + strconv.Itoa(beego.BConfig.Listen.HTTPPort)
-
-	res := componenttest.SendHTTP(
-		"POST",
-		host+"/template/v1/template",
-		[]byte(`{
-			"rqBody":{
-			}
-		}`),
-	)
-
-	Convey("TestTemplateFailed", t, func() {
-		Convey("Should Failed", func() {
-			So(len(res.Error), ShouldEqual, 1)
 		})
 	})
 }
